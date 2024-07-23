5dba9084fbe572f2563f387b0e2afad9598bc9dd
KMcGoldrick <kevinmcgoldrick12@gmail.com>
2024-07-23 10:00:46 -0400

    Added BMM150 int.

--- BMMTest2/BMMTest2.ino
+++ BMMTest2/BMMTest2.ino
@@ -1,7 +1,9 @@
 #include <bmm150_defs.h>
 #include <bmm150.h>
 int loopCnt;
+int8_t rslt;
 float x, y, z;
+bmm150_dev MyBMM150;
 
 void serialData() {
     Serial.print(0);
@@ -19,6 +21,11 @@ void setup() {
     Serial.begin(9600);
     while (!Serial);
     Serial.println("Started");
+
+    rslt = bmm150_init(&MyBMM150);
+    Serial.print("bmm150_init: ");Serial.println(rslt);
+
+    while (true) {}
 }
 
 void loop() {
