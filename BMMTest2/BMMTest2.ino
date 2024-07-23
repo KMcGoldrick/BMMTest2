7662218620975e12b6f086301371e89ae11aedd4
KMcGoldrick <kevinmcgoldrick12@gmail.com>
2024-07-23 16:43:37 -0400

    Much progress.

--- BMMTest2/BMMTest2.ino
+++ BMMTest2/BMMTest2.ino
@@ -34,14 +34,9 @@ void serialData() {
     Serial.println(z);
 }
 
-void my_bmm150_error_codes_print_result(char funcName[], int8_t result) {
-    Serial.print(funcName);
-    Serial.println(result);
-}
-
 uint8_t my_burst_read(uint8_t reg_addr, uint8_t* reg_data, int length) {
     // Start a transmission to the I2C device
-    Serial.print("my_burst_read ");Serial.print(dev_addr, HEX);Serial.print(reg_addr, HEX);
+    Serial.print("     my_burst_read  ");Serial.print(dev_addr, HEX);Serial.print(reg_addr, HEX);
     Wire.beginTransmission(dev_addr);
     Wire.write(reg_addr);  // Send the register address
     Wire.endTransmission(false);  // End the transmission without releasing the bus
@@ -68,7 +63,7 @@ uint8_t my_burst_read(uint8_t reg_addr, uint8_t* reg_data, int length) {
 
 uint8_t my_burst_write(uint8_t reg_addr, const uint8_t* reg_data, int length) {
     // Perform the burst write
-    Serial.print("my_burst_write ");Serial.print(dev_addr, HEX);Serial.print(reg_addr, HEX);
+    Serial.print("     my_burst_write ");Serial.print(dev_addr, HEX);Serial.print(reg_addr, HEX);
     // Print the read data
     Serial.print(" Data to write: ");
     for (uint8_t i = 0; i < length; i++) {
@@ -92,93 +87,7 @@ uint8_t my_burst_write(uint8_t reg_addr, const uint8_t* reg_data, int length) {
     return error;
 }
 
-void my_bmm150_user_delay_us(uint32_t period_us, void* intf_ptr)
-{
-    delay(1000*period_us);
-}
-
-int8_t my_bmm150_user_i2c_reg_write(uint8_t reg_addr, const uint8_t* reg_data, uint32_t length,void* intf_ptr)
-{
-    /*
-    const struct device* i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2csensor));
-    if (!i2c_dev) {
-        return -1;
-    }
-
-    uint8_t dev_addr = *(uint8_t*)intf_ptr;
-    */
-    return my_burst_write(reg_addr, reg_data, length);
-}
-
-int8_t my_bmm150_user_i2c_reg_read(uint8_t reg_addr, uint8_t* reg_data, uint32_t length,void* intf_ptr)
-{
-    /*
-    const struct device* i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2csensor));
-    if (!i2c_dev) {
-        return -1;
-    }
-
-    uint8_t dev_addr = *(uint8_t*)intf_ptr;
-    */
-    my_burst_read(reg_addr, reg_data, length);
-    return 0;
-}
-
-int8_t my_bmm150_get_regs(uint8_t reg_addr, uint8_t* reg_data, uint32_t len, struct bmm150_dev* dev)
-{
-    int8_t rslt;
-
-    /* Check for null pointer in the device structure */
-    rslt = BMM150_OK;//KJM null_ptr_check(dev);
-
-    /* Proceed if null check is fine */
-    if ((rslt == BMM150_OK) && (reg_data != NULL))
-    {
-        if (dev->intf != BMM150_I2C_INTF)
-        {
-            /* If interface selected is SPI */
-            reg_addr = reg_addr | 0x80;
-        }
-
-        /* Read the data from the reg_addr */
-        rslt = dev->read(reg_addr, reg_data, len, dev->intf_ptr);
-    }
-    else
-    {
-        rslt = BMM150_E_NULL_PTR;
-    }
-
-    my_bmm150_error_codes_print_result("my_bmm150_get_regs: ", rslt);
-    return rslt;
-}
-
-int8_t my_bmm150_set_regs(uint8_t reg_addr, const uint8_t* reg_data, uint32_t len, struct bmm150_dev* dev)
-{
-    int8_t rslt;
-
-    /* Check for null pointer in the device structure */
-    rslt = BMM150_OK;//KJM null_ptr_check(dev);
-
-    /* Proceed if null check is fine */
-    if ((rslt == BMM150_OK) && (reg_data != NULL) && (len != 0))
-    {
-        /* Write the data to the reg_addr */
-
-        /* SPI write requires to set The MSB of reg_addr as 0
-         * but in default the MSB is always 0
-         */
-        dev->intf_rslt = dev->write(reg_addr, reg_data, len, dev->intf_ptr);
-    }
-    else
-    {
-        rslt = BMM150_E_NULL_PTR;
-    }
-
-    my_bmm150_error_codes_print_result("my_bmm150_set_regs: ", rslt);
-    return rslt;
-}
-
-static int8_t my_suspend_to_sleep_mode(struct bmm150_dev* dev)
+int8_t my_suspend_to_sleep_mode(struct bmm150_dev* dev)
 {
     int8_t rslt;
 
@@ -200,7 +109,7 @@ static int8_t my_suspend_to_sleep_mode(struct bmm150_dev* dev)
     return rslt;
 }
 
-static int8_t my_write_op_mode(uint8_t op_mode, struct bmm150_dev* dev)
+int8_t my_write_op_mode(uint8_t op_mode, struct bmm150_dev* dev)
 {
     int8_t rslt;
     uint8_t reg_data;
@@ -218,7 +127,7 @@ static int8_t my_write_op_mode(uint8_t op_mode, struct bmm150_dev* dev)
     return rslt;
 }
 
-static uint8_t my_are_settings_changed(uint16_t sub_settings, uint16_t desired_settings)
+uint8_t my_are_settings_changed(uint16_t sub_settings, uint16_t desired_settings)
 {
     uint8_t settings_changed;
 
@@ -236,7 +145,7 @@ static uint8_t my_are_settings_changed(uint16_t sub_settings, uint16_t desired_s
     return settings_changed;
 }
 
-static int8_t my_set_control_measurement_xyz(const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_set_control_measurement_xyz(const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt;
     uint8_t reg_data;
@@ -253,7 +162,7 @@ static int8_t my_set_control_measurement_xyz(const struct bmm150_settings* setti
     return rslt;
 }
 
-static int8_t my_mode_settings(uint16_t desired_settings, const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_mode_settings(uint16_t desired_settings, const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt = BMM150_E_INVALID_CONFIG;
 
@@ -284,7 +193,7 @@ static int8_t my_mode_settings(uint16_t desired_settings, const struct bmm150_se
     return rslt;
 }
 
-static int8_t my_interrupt_pin_settings(uint16_t desired_settings,
+int8_t my_interrupt_pin_settings(uint16_t desired_settings,
     const struct bmm150_settings* settings,
     struct bmm150_dev* dev)
 {
@@ -336,7 +245,7 @@ static int8_t my_interrupt_pin_settings(uint16_t desired_settings,
     return rslt;
 }
 
-static int8_t my_interrupt_config(uint16_t desired_settings, const struct bmm150_settings* settings,
+int8_t my_interrupt_config(uint16_t desired_settings, const struct bmm150_settings* settings,
     struct bmm150_dev* dev)
 {
     int8_t rslt;
@@ -379,7 +288,7 @@ static int8_t my_interrupt_config(uint16_t desired_settings, const struct bmm150
     return rslt;
 }
 
-static int8_t my_interrupt_threshold_settings(uint16_t desired_settings,
+int8_t my_interrupt_threshold_settings(uint16_t desired_settings,
     const struct bmm150_settings* settings,
     struct bmm150_dev* dev)
 {
@@ -403,7 +312,7 @@ static int8_t my_interrupt_threshold_settings(uint16_t desired_settings,
     return rslt;
 }
 
-static int8_t my_set_xy_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_set_xy_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt;
     uint8_t rep_xy;
@@ -415,7 +324,7 @@ static int8_t my_set_xy_rep(const struct bmm150_settings* settings, struct bmm15
     return rslt;
 }
 
-static int8_t my_set_z_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_set_z_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt;
     uint8_t rep_z;
@@ -427,7 +336,7 @@ static int8_t my_set_z_rep(const struct bmm150_settings* settings, struct bmm150
     return rslt;
 }
 
-static int8_t my_set_odr(const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_set_odr(const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt;
     uint8_t reg_data;
@@ -445,7 +354,7 @@ static int8_t my_set_odr(const struct bmm150_settings* settings, struct bmm150_d
     return rslt;
 }
 
-static int8_t my_set_odr_xyz_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
+int8_t my_set_odr_xyz_rep(const struct bmm150_settings* settings, struct bmm150_dev* dev)
 {
     int8_t rslt;
 
@@ -464,6 +373,200 @@ static int8_t my_set_odr_xyz_rep(const struct bmm150_settings* settings, struct 
         }
     }
 
+    return rslt;
+}
+
+int8_t my_set_config(struct bmm150_dev* dev)
+{
+    /* Status of api are returned to this variable. */
+    int8_t rslt;
+
+    struct bmm150_settings settings;
+
+    /* Set powermode as normal mode */
+    settings.pwr_mode = BMM150_POWERMODE_NORMAL;
+    rslt = my_bmm150_set_op_mode(&settings, dev);
+
+    if (rslt == BMM150_OK)
+    {
+        settings.preset_mode = BMM150_PRESETMODE_LOWPOWER;
+        rslt = my_bmm150_set_presetmode(&settings, dev);
+
+        if (rslt == BMM150_OK)
+        {
+            /* Map the data interrupt pin */
+            settings.int_settings.drdy_pin_en = 0x01;
+            rslt = my_bmm150_set_sensor_settings(BMM150_SEL_DRDY_PIN_EN, &settings, dev);
+        }
+    }
+
+    my_bmm150_error_codes_print_result("my_set_config: ", rslt);
+    return rslt;
+}
+
+int8_t my_set_power_control_bit(uint8_t pwrcntrl_bit, struct bmm150_dev* dev)
+{
+    int8_t rslt;
+    uint8_t reg_data = 0;
+
+    /* Power control register 0x4B is read */
+    rslt = my_bmm150_get_regs(BMM150_REG_POWER_CONTROL, &reg_data, 1, dev);
+
+    /* Proceed if everything is fine until now */
+    if (rslt == BMM150_OK)
+    {
+        /* Sets the value of power control bit */
+        reg_data = BMM150_SET_BITS_POS_0(reg_data, BMM150_PWR_CNTRL, pwrcntrl_bit);
+        rslt = my_bmm150_set_regs(BMM150_REG_POWER_CONTROL, &reg_data, 1, dev);
+
+        if (rslt == BMM150_OK)
+        {
+            /* Store the power control bit
+             * value in dev structure
+             */
+            dev->pwr_cntrl_bit = pwrcntrl_bit;
+        }
+    }
+
+    my_bmm150_error_codes_print_result("my_set_power_control_bit: ", rslt);
+    return rslt;
+}
+
+int8_t my_read_trim_registers(struct bmm150_dev* dev)
+{
+    int8_t rslt;
+    uint8_t trim_x1y1[2] = { 0 };
+    uint8_t trim_xyz_data[4] = { 0 };
+    uint8_t trim_xy1xy2[10] = { 0 };
+    uint16_t temp_msb = 0;
+
+    /* Trim register value is read */
+    rslt = bmm150_get_regs(BMM150_DIG_X1, trim_x1y1, 2, dev);
+
+    if (rslt == BMM150_OK)
+    {
+        rslt = bmm150_get_regs(BMM150_DIG_Z4_LSB, trim_xyz_data, 4, dev);
+
+        if (rslt == BMM150_OK)
+        {
+            rslt = bmm150_get_regs(BMM150_DIG_Z2_LSB, trim_xy1xy2, 10, dev);
+
+            if (rslt == BMM150_OK)
+            {
+                /* Trim data which is read is updated
+                 * in the device structure
+                 */
+                dev->trim_data.dig_x1 = (int8_t)trim_x1y1[0];
+                dev->trim_data.dig_y1 = (int8_t)trim_x1y1[1];
+                dev->trim_data.dig_x2 = (int8_t)trim_xyz_data[2];
+                dev->trim_data.dig_y2 = (int8_t)trim_xyz_data[3];
+                temp_msb = ((uint16_t)trim_xy1xy2[3]) << 8;
+                dev->trim_data.dig_z1 = (uint16_t)(temp_msb | trim_xy1xy2[2]);
+                temp_msb = ((uint16_t)trim_xy1xy2[1]) << 8;
+                dev->trim_data.dig_z2 = (int16_t)(temp_msb | trim_xy1xy2[0]);
+                temp_msb = ((uint16_t)trim_xy1xy2[7]) << 8;
+                dev->trim_data.dig_z3 = (int16_t)(temp_msb | trim_xy1xy2[6]);
+                temp_msb = ((uint16_t)trim_xyz_data[1]) << 8;
+                dev->trim_data.dig_z4 = (int16_t)(temp_msb | trim_xyz_data[0]);
+                dev->trim_data.dig_xy1 = trim_xy1xy2[9];
+                dev->trim_data.dig_xy2 = (int8_t)trim_xy1xy2[8];
+                temp_msb = ((uint16_t)(trim_xy1xy2[5] & 0x7F)) << 8;
+                dev->trim_data.dig_xyz1 = (uint16_t)(temp_msb | trim_xy1xy2[4]);
+            }
+        }
+    }
+
+    return rslt;
+}
+
+void my_bmm150_error_codes_print_result(char funcName[], int8_t result) {
+    Serial.print(funcName);
+    Serial.println(result);
+}
+
+void my_bmm150_user_delay_us(uint32_t period_us, void* intf_ptr)
+{
+    delay(period_us);
+}
+
+int8_t my_bmm150_user_i2c_reg_write(uint8_t reg_addr, const uint8_t* reg_data, uint32_t length, void* intf_ptr)
+{
+    /*
+    const struct device* i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2csensor));
+    if (!i2c_dev) {
+        return -1;
+    }
+
+    uint8_t dev_addr = *(uint8_t*)intf_ptr;
+    */
+    return my_burst_write(reg_addr, reg_data, length);
+}
+
+int8_t my_bmm150_user_i2c_reg_read(uint8_t reg_addr, uint8_t* reg_data, uint32_t length, void* intf_ptr)
+{
+    /*
+    const struct device* i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2csensor));
+    if (!i2c_dev) {
+        return -1;
+    }
+
+    uint8_t dev_addr = *(uint8_t*)intf_ptr;
+    */
+    my_burst_read(reg_addr, reg_data, length);
+    return 0;
+}
+
+int8_t my_bmm150_get_regs(uint8_t reg_addr, uint8_t* reg_data, uint32_t len, struct bmm150_dev* dev)
+{
+    int8_t rslt;
+
+    /* Check for null pointer in the device structure */
+    rslt = BMM150_OK;//KJM null_ptr_check(dev);
+
+    /* Proceed if null check is fine */
+    if ((rslt == BMM150_OK) && (reg_data != NULL))
+    {
+        if (dev->intf != BMM150_I2C_INTF)
+        {
+            /* If interface selected is SPI */
+            reg_addr = reg_addr | 0x80;
+        }
+
+        /* Read the data from the reg_addr */
+        rslt = dev->read(reg_addr, reg_data, len, dev->intf_ptr);
+    }
+    else
+    {
+        rslt = BMM150_E_NULL_PTR;
+    }
+
+    //KJMmy_bmm150_error_codes_print_result("my_bmm150_get_regs: ", rslt);
+    return rslt;
+}
+
+int8_t my_bmm150_set_regs(uint8_t reg_addr, const uint8_t* reg_data, uint32_t len, struct bmm150_dev* dev)
+{
+    int8_t rslt;
+
+    /* Check for null pointer in the device structure */
+    rslt = BMM150_OK;//KJM null_ptr_check(dev);
+
+    /* Proceed if null check is fine */
+    if ((rslt == BMM150_OK) && (reg_data != NULL) && (len != 0))
+    {
+        /* Write the data to the reg_addr */
+
+        /* SPI write requires to set The MSB of reg_addr as 0
+         * but in default the MSB is always 0
+         */
+        dev->intf_rslt = dev->write(reg_addr, reg_data, len, dev->intf_ptr);
+    }
+    else
+    {
+        rslt = BMM150_E_NULL_PTR;
+    }
+
+    //KJMmy_bmm150_error_codes_print_result("my_bmm150_set_regs: ", rslt);
     return rslt;
 }
 
@@ -533,7 +636,7 @@ int8_t my_bmm150_set_op_mode(const struct bmm150_settings* settings, struct bmm1
         }
     }
 
-    my_bmm150_error_codes_print_result("my_bmm150_set_op_mode", rslt);
+    my_bmm150_error_codes_print_result("my_bmm150_set_op_mode: ", rslt);
     return rslt;
 }
 
@@ -574,6 +677,40 @@ int8_t my_bmm150_set_sensor_settings(uint16_t desired_settings,
         }
     }
 
+    return rslt;
+}
+
+int8_t my_bmm150_init(struct bmm150_dev* dev)
+{
+    int8_t rslt;
+    uint8_t chip_id = 0;
+
+    /* Power up the sensor from suspend to sleep mode */
+    rslt = my_set_power_control_bit(BMM150_POWER_CNTRL_ENABLE, dev);
+
+    if (rslt == BMM150_OK)
+    {
+        /* Start-up time delay of 3ms */
+        dev->delay_us(BMM150_START_UP_TIME, dev->intf_ptr);
+
+        /* Chip ID of the sensor is read */
+        rslt = my_bmm150_get_regs(BMM150_REG_CHIP_ID, &chip_id, 1, dev);
+
+        /* Proceed if everything is fine until now */
+        if (rslt == BMM150_OK)
+        {
+            /* Check for chip id validity */
+            if (chip_id == BMM150_CHIP_ID)
+            {
+                dev->chip_id = chip_id;
+
+                /* Function to update trim values */
+                rslt = my_read_trim_registers(dev);
+            }
+        }
+    }
+
+    my_bmm150_error_codes_print_result("my_bmm150_init: ", rslt);
     return rslt;
 }
 
@@ -638,143 +775,6 @@ int8_t my_bmm150_set_presetmode(struct bmm150_settings* settings, struct bmm150_
         }
     }
 
-    return rslt;
-}
-
-int8_t my_set_config(struct bmm150_dev* dev)
-{
-    /* Status of api are returned to this variable. */
-    int8_t rslt;
-
-    struct bmm150_settings settings;
-
-    /* Set powermode as normal mode */
-    settings.pwr_mode = BMM150_POWERMODE_NORMAL;
-    rslt = my_bmm150_set_op_mode(&settings, dev);
-
-    if (rslt == BMM150_OK)
-    {
-        settings.preset_mode = BMM150_PRESETMODE_LOWPOWER;
-        rslt = my_bmm150_set_presetmode(&settings, dev);
-
-        if (rslt == BMM150_OK)
-        {
-            /* Map the data interrupt pin */
-            settings.int_settings.drdy_pin_en = 0x01;
-            rslt = my_bmm150_set_sensor_settings(BMM150_SEL_DRDY_PIN_EN, &settings, dev);
-        }
-    }
-
-    my_bmm150_error_codes_print_result("my_set_config", rslt);
-    return rslt;
-}
-
-int8_t my_set_power_control_bit(uint8_t pwrcntrl_bit, struct bmm150_dev* dev)
-{
-    int8_t rslt;
-    uint8_t reg_data = 0;
-
-    /* Power control register 0x4B is read */
-    rslt = my_bmm150_get_regs(BMM150_REG_POWER_CONTROL, &reg_data, 1, dev);
-
-    /* Proceed if everything is fine until now */
-    if (rslt == BMM150_OK)
-    {
-        /* Sets the value of power control bit */
-        reg_data = BMM150_SET_BITS_POS_0(reg_data, BMM150_PWR_CNTRL, pwrcntrl_bit);
-        rslt = my_bmm150_set_regs(BMM150_REG_POWER_CONTROL, &reg_data, 1, dev);
-
-        if (rslt == BMM150_OK)
-        {
-            /* Store the power control bit
-             * value in dev structure
-             */
-            dev->pwr_cntrl_bit = pwrcntrl_bit;
-        }
-    }
-
-    my_bmm150_error_codes_print_result("my_set_power_control_bit: ", rslt);
-    return rslt;
-}
-
-static int8_t my_read_trim_registers(struct bmm150_dev* dev)
-{
-    int8_t rslt;
-    uint8_t trim_x1y1[2] = { 0 };
-    uint8_t trim_xyz_data[4] = { 0 };
-    uint8_t trim_xy1xy2[10] = { 0 };
-    uint16_t temp_msb = 0;
-
-    /* Trim register value is read */
-    rslt = bmm150_get_regs(BMM150_DIG_X1, trim_x1y1, 2, dev);
-
-    if (rslt == BMM150_OK)
-    {
-        rslt = bmm150_get_regs(BMM150_DIG_Z4_LSB, trim_xyz_data, 4, dev);
-
-        if (rslt == BMM150_OK)
-        {
-            rslt = bmm150_get_regs(BMM150_DIG_Z2_LSB, trim_xy1xy2, 10, dev);
-
-            if (rslt == BMM150_OK)
-            {
-                /* Trim data which is read is updated
-                 * in the device structure
-                 */
-                dev->trim_data.dig_x1 = (int8_t)trim_x1y1[0];
-                dev->trim_data.dig_y1 = (int8_t)trim_x1y1[1];
-                dev->trim_data.dig_x2 = (int8_t)trim_xyz_data[2];
-                dev->trim_data.dig_y2 = (int8_t)trim_xyz_data[3];
-                temp_msb = ((uint16_t)trim_xy1xy2[3]) << 8;
-                dev->trim_data.dig_z1 = (uint16_t)(temp_msb | trim_xy1xy2[2]);
-                temp_msb = ((uint16_t)trim_xy1xy2[1]) << 8;
-                dev->trim_data.dig_z2 = (int16_t)(temp_msb | trim_xy1xy2[0]);
-                temp_msb = ((uint16_t)trim_xy1xy2[7]) << 8;
-                dev->trim_data.dig_z3 = (int16_t)(temp_msb | trim_xy1xy2[6]);
-                temp_msb = ((uint16_t)trim_xyz_data[1]) << 8;
-                dev->trim_data.dig_z4 = (int16_t)(temp_msb | trim_xyz_data[0]);
-                dev->trim_data.dig_xy1 = trim_xy1xy2[9];
-                dev->trim_data.dig_xy2 = (int8_t)trim_xy1xy2[8];
-                temp_msb = ((uint16_t)(trim_xy1xy2[5] & 0x7F)) << 8;
-                dev->trim_data.dig_xyz1 = (uint16_t)(temp_msb | trim_xy1xy2[4]);
-            }
-        }
-    }
-
-    return rslt;
-}
-
-int8_t my_bmm150_init(struct bmm150_dev* dev)
-{
-    int8_t rslt;
-    uint8_t chip_id = 0;
-
-    /* Power up the sensor from suspend to sleep mode */
-    rslt = my_set_power_control_bit(BMM150_POWER_CNTRL_ENABLE, dev);
-
-    if (rslt == BMM150_OK)
-    {
-        /* Start-up time delay of 3ms */
-        dev->delay_us(BMM150_START_UP_TIME, dev->intf_ptr);
-
-        /* Chip ID of the sensor is read */
-        rslt = my_bmm150_get_regs(BMM150_REG_CHIP_ID, &chip_id, 1, dev);
-
-        /* Proceed if everything is fine until now */
-        if (rslt == BMM150_OK)
-        {
-            /* Check for chip id validity */
-            if (chip_id == BMM150_CHIP_ID)
-            {
-                dev->chip_id = chip_id;
-
-                /* Function to update trim values */
-                rslt = my_read_trim_registers(dev);
-            }
-        }
-    }
-
-    my_bmm150_error_codes_print_result("my_bmm150_init: ", rslt);
     return rslt;
 }
 
